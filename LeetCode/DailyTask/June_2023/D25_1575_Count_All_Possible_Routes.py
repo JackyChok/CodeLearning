f337a5fc50f306f1df575be3018b4fdb229c7803
JackyChok <jackychok810@gmail.com>
2023-06-25 21:23:23 +0800

    Day 25 All Possible Routes

---
+++ LeetCode/DailyTask/June_2023/D25_1575_Count_All_Possible_Routes.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def countRoutes(self, locations, start, finish, fuel):
+        """
+        :type locations: List[int]
+        :type start: int
+        :type finish: int
+        :type fuel: int
+        :rtype: int
+        """
+        def dfs(curr_loc, rem_fuel):
+            if rem_fuel < 0:
+                return 0
+            elif curr_loc == finish:
+                res = 1
+            else:
+                res =  0
+
+            for next_loc in range(len(locations)):
+                if curr_loc != next_loc:
+                    cost = abs(locations[curr_loc] - locations[next_loc])
+                    res += dfs(next_loc, rem_fuel - cost)
+            return res
+
+        return dfs(start, fuel)
+    
+# Test Case 1
+locations = [2, 3, 6, 8, 4]
+start = 1
+finish = 3
+fuel = 5
+print(Solution().countRoutes(locations, start, finish, fuel))    
+
+# Test Case 2
+locations = [4, 3, 1]
+start = 1
+finish = 0
+fuel = 6
+print(Solution().countRoutes(locations, start, finish, fuel))
+
+# Test Case 2
+locations = [5, 2, 1]
+start = 0
+finish = 2
+fuel = 3
+print(Solution().countRoutes(locations, start, finish, fuel)) 
