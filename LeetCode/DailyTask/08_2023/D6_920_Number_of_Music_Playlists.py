1c51acc8f455c0c5a13565380c1bddea13350333
JackyChok <jackychok810@gmail.com>
2023-08-07 15:45:41 +0800

    Day 6 August with Comment

--- LeetCode/DailyTask/08_2023/D6_920_Number_of_Music_Playlists.py
+++ LeetCode/DailyTask/08_2023/D6_920_Number_of_Music_Playlists.py
@@ -1,15 +1,31 @@
 class Solution(object):
     def numMusicPlaylists(self, n, goal, k):
+        """
+        :type n: int
+        :type goal: int
+        :type k: int
+        :rtype: int
+        """
+        # Define the constant MOD to be used for modulo operations
         MOD = 10**9 + 7
+        
+        # Initialize a two-dimensional array dp with all elements set to 0
         dp = [[0] * (n + 1) for _ in range(goal + 1)]
+        
+        # There is only one way to create an empty playlist
         dp[0][0] = 1
 
+        # Loop through the elements of the dp array
         for i in range(1, goal + 1):
             for j in range(1, n + 1):
+                # Calculate dp[i][j] using the formula for adding a new song to the playlist
                 dp[i][j] = (dp[i-1][j-1] * (n - j + 1)) % MOD
+                
+                # If j is greater than k, consider the case when we can repeat songs
                 if j > k:
                     dp[i][j] = (dp[i][j] + dp[i-1][j] * (j - k)) % MOD
 
+        # The value in dp[goal][n] represents the number of possible playlists
         return dp[goal][n]
 
 def run_tests():
